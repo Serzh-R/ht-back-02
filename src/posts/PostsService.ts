11576bae71f0a78ba1fcd834c7a1c2e56e77c897
Serzh-R <daarksen2@gmail.com>
2025-01-14 16:22:31 +0500

    clear PostsService.ts

--- src/posts/PostsService.ts
+++ src/posts/PostsService.ts
@@ -69,32 +69,4 @@ export const postsService = {
   async deletePost(id: string): Promise<boolean> {
     return await postsRepository.deletePost(new ObjectId(id).toString())
   },
-
-  /*async createPostForBlog(blogId: string, post: BlogPostInputType): Promise<PostType | null> {
-    const blog = await blogsQueryRepository.getBlogById(blogId)
-    if (!blog) {
-      return null
-    }
-
-    return await this._createNewPost({
-      title: post.title,
-      shortDescription: post.shortDescription,
-      content: post.content,
-      blogId: blogId,
-      blogName: blog.name,
-    })
-  },*/
-
-  /*async _createNewPost(post: Omit<PostType, 'id' | 'createdAt'>): Promise<PostType> {
-    const newPost: PostDBInsertType = {
-      title: post.title,
-      shortDescription: post.shortDescription,
-      content: post.content,
-      blogId: post.blogId,
-      blogName: post.blogName,
-      createdAt: new Date().toISOString(),
-    }
-
-    return await postsRepository.createPost(newPost)
-  },*/
 }
