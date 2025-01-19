b3f85dc310ba3b4a5d3e435591a5c1749b2a7b60
Serzh-R <daarksen2@gmail.com>
2025-01-20 02:01:48 +0500

    fix UsersService.ts

--- src/users/UsersService.ts
+++ src/users/UsersService.ts
@@ -1,7 +1,7 @@
-import { FieldErrorType } from '../types/types'
 import { usersRepository } from './UsersRepository'
 import { bcryptService } from '../common/adapters/bcrypt.service'
 import { UserDBInsertType, UserInputType } from '../auth/types/types'
+import { FieldErrorType } from '../types/types'
 
 export const usersService = {
   async createUser(
@@ -49,6 +49,45 @@ export const usersService = {
       errorsMessages: [],
     }
   },
+
+  /*async createUser(body: UserInputType): Promise<Result<UserType | null>> {
+    const userByLogin = await usersRepository.findByLoginOrEmail(body.login)
+    if (userByLogin) {
+      return {
+        status: ResultStatus.Forbidden,
+        errorMessage: 'Login already exists',
+        extensions: [{ field: 'login', message: 'login should be unique' }],
+        data: null,
+      }
+    }
+
+    const userByEmail = await usersRepository.findByLoginOrEmail(body.email)
+    if (userByEmail) {
+      return {
+        status: ResultStatus.Forbidden,
+        errorMessage: 'Email already exists',
+        extensions: [{ field: 'email', message: 'email should be unique' }],
+        data: null,
+      }
+    }
+
+    const { passwordHash } = await bcryptService.generateHash(body.password)
+
+    const userDB: UserDBInsertType = {
+      login: body.login,
+      email: body.email,
+      passwordHash,
+      createdAt: new Date(),
+    }
+
+    const user = await usersRepository.createUser(userDB)
+
+    return {
+      status: ResultStatus.Success,
+      data: user,
+      extensions: [],
+    }
+  },*/
 
   async deleteUser(id: string): Promise<boolean> {
     return await usersRepository.deleteUser(id)
