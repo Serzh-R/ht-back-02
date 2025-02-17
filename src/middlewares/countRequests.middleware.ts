9cd0c30c1d0a139e94bac993b82a4ee7f7feabd0
Serzh-R <daarksen2@gmail.com>
2025-02-18 00:48:42 +0500

    fix2 countRequests.middleware.ts

--- src/middlewares/countRequests.middleware.ts
+++ src/middlewares/countRequests.middleware.ts
@@ -4,7 +4,7 @@ import { HTTP_STATUSES } from '../settings'
 
 export const countRequestsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { IP, URL } = req.query
+    const { IP } = req.query
     const tenSecondsAgo = new Date(Date.now() - 10 * 1000) // 10 секунд назад
 
     const filter: any = {
@@ -13,15 +13,12 @@ export const countRequestsMiddleware = async (req: Request, res: Response, next:
 
     if (IP) {
       filter.IP = String(IP)
-    }
-
-    if (URL) {
-      filter.URL = String(URL)
+    } else {
+      filter.IP = req.ip // Используем IP запроса, если IP не передан в query
     }
 
     const count = await requestsCollection.countDocuments(filter)
 
-    // Если количество запросов больше 5, возвращаем ошибку 429
     if (count >= 5) {
       res.status(HTTP_STATUSES.TOO_MANY_REQUESTS_429).json({
         error: 'Too Many Requests',
@@ -33,9 +30,20 @@ export const countRequestsMiddleware = async (req: Request, res: Response, next:
 
     res.locals.count = count
 
+    // Сохраняем запрос в базе данных (IP, URL, дата запроса)
+    await requestsCollection.insertOne({
+      IP: req.ip ?? '',
+      URL: req.originalUrl,
+      date: new Date(), // Сохраняем текущую дату
+    })
+
     next()
   } catch (error) {
     console.error('Error counting requests:', error)
     res.status(500).json({ error: 'Internal Server Error' })
   }
 }
+
+/*if (URL) {
+      filter.URL = String(URL)
+    }*/
