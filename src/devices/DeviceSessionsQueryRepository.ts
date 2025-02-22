b7ce8ebb1f0d4cae6aabe4484959a131a392a514
Serzh-R <daarksen2@gmail.com>
2025-02-22 14:00:04 +0500

    fix getDevicesByUserId

--- src/devices/DeviceSessionsQueryRepository.ts
+++ src/devices/DeviceSessionsQueryRepository.ts
@@ -3,15 +3,19 @@ import { deviceSessionsCollection } from '../db/mongoDb'
 
 export const deviceSessionsQueryRepository = {
   async getDevicesByUserId(userId: string): Promise<DeviceSessionType[]> {
-    const currentTime = Date.now()
-
-    // ✅ Фильтруем только активные устройства
     const devices = await deviceSessionsCollection
       .find({
         userId,
-        expirationDate: { $gt: currentTime }, // Сессии, которые ещё не истекли
       })
       .toArray()
+
+    /*const currentTime = Date.now()
+    const devices = await deviceSessionsCollection
+      .find({
+        userId,
+        expirationDate: { $gt: currentTime },
+      })
+      .toArray()*/
 
     return devices.map(
       (device: DeviceSessionDBType): DeviceSessionType => ({
